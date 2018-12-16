e112eeb85e5f69a4c56416445920309e037e8fb3
Vadim Rybak <vadim82@gmail.com>
2018-12-16 14:01:11 -0500

    more comments

--- Program.cs
+++ Program.cs
@@ -17,18 +17,20 @@ namespace MagazineAPI
 
             var tSubs = client.GetSubscribersAsync(token);
 
+            // Gets a list of Tasks for each category.
             var tMags = categories
                 .Select(cat => client.GetMagazinesAsync(token, cat))
                 .ToList();
-
+            // take the subs task and all the magazine tasks and wait for all of them to complete
             var parallelTasks = new List<Task> { tSubs }.Concat(tMags).ToArray();
             await Task.WhenAll(parallelTasks);
 
             // hashmap of magezineId -> categoryId
             var magToCatMap = tMags
-                .SelectMany(t => t.Result)
-                .ToDictionary(t => t.Id, t => t.Category);
+                .SelectMany(t => t.Result) // Select many is to flatten an array of arrays. [ [1,2], [2,3], [5,6]  ] => [1,2,2,3,5,6]. Similar to calling .flatten().map()
+                .ToDictionary(t => t.Id, t => t.Category); // converts a list to a dictionary. First lambda is the key code block, second lambda is the value code block
 
+            // give me all the subscribers where the persons distinct category count is the same as the number of categories.
             var matchingSubIds = tSubs.Result
                 .Where(s => s.MagazineIds.Select(m => magToCatMap[m]).Distinct().Count() == categories.Count)
                 .Select(s => s.Id)
